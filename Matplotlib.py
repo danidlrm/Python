18648b8bcf8d7f6f541da90f2129acfc9bc9f18d
Daniela Dávila Román <125614804+danidlrm@users.noreply.github.com>
2025-04-02 17:48:56 -0600

    Update Matplotlib.py

--- Matplotlib.py
+++ Matplotlib.py
@@ -1,1 +1,8 @@
+'''
+MATPLOTLIB
+==========
+Matplotlib is a low level graph plotting library in python that serves as a visualization utility. It was created by John D. Hunter. Is open source and we can use it freely.
+Matplotlib is mostly written in python, a few segments are written in C, Objective-C and Javascript for Platform compatibility.
+
+'''
 
